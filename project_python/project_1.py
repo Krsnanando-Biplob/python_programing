683573cebfad28f5c90eca66b3b8498aa61a6310
Krishnando Biplob <krsnanandobiplob@gmail.com>
2026-04-14 10:10:45 +0700

    streamilt img management

--- project_python/project_1.py
+++ project_python/project_1.py
@@ -7,12 +7,36 @@ st.title(":orange[**Hello Biplob**]", anchor=False)
 
 st.markdown("my name is biplob")
 
-name = st.text_input("enter your name")
-st.write("your name:",name)
-age = st.number_input("enter your age",value= None)
-st.write("your age:", age)
+name = st.text_input("enter your name", placeholder="write your name...")
+# st.write("your name:",name)
+age = st.number_input("enter your age",value= None, placeholder="write your age...")
+# st.write("your age:", age)
+pas = st.text_input("enter your password",type="password",placeholder="write your password...")
 
-passa = st.button("enter to confrim")
+passa = st.button("enter to confrim",type="primary")
 
 if passa:
     st.write(f"your name {name} your age {age}")
+    
+seletedd = st.selectbox("your select profation ",
+                        ("student","teacher","jobholder"),
+                        index = None,
+                        accept_new_options =True
+                        )
+
+
+st.write("selection :",seletedd)
+
+print(type(seletedd))
+
+
+imag = st.file_uploader("enter your img", type=["jpg",'jpeg',"png"],
+                        accept_multiple_files = True)
+print(type(imag))
+
+if imag:
+    col = st.columns(len(imag))
+    for i, img in enumerate(imag):
+        col[i].image(img)
+
+
